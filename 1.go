eaae7323fb2e6d9565cb4b1631c6646f01e701dd
srinathvissarapu <119414830+srinathvissarapu@users.noreply.github.com>
2023-03-19 11:38:07 +0530

    Update 1.go

--- 1.go
+++ 1.go
@@ -20,8 +20,6 @@ func main() {
 func isEven(num int) bool {
 	return num%2 == 0
 }
-In this program, we use the same fmt and math/rand packages as before, but we generate a random number between 1 and 100 using rand.Intn(100) + 1 instead of rand.Intn(10). We then call a custom function isEven to check if the number is even or odd, and print a message accordingly. This is a different method than the original code, which simply printed the random number.
-
 
 
 
