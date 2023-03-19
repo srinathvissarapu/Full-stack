cbe41eaf4306b3a97ec55d5ce0e8233b6e59d101
srinathvissarapu <119414830+srinathvissarapu@users.noreply.github.com>
2023-03-19 11:26:42 +0530

    Update 13.go

--- 13.go
+++ 13.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	var x, y int = 3, 4
-	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var f float64 = math.Pow(float64(x), 2) + math.Pow(float64(y), 2)
 	var z uint = uint(f)
 	fmt.Println(x, y, z)
 }
