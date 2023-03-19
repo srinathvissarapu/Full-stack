b3ecee8ac95a9332dc0bc56ec075bf2d4d35b877
srinathvissarapu <119414830+srinathvissarapu@users.noreply.github.com>
2023-03-19 11:10:48 +0530

    Update 1.go

--- 1.go
+++ 1.go
@@ -6,5 +6,24 @@ import (
 )
 
 func main() {
-	fmt.Println("My favorite number is", rand.Intn(10))
+	
+	randomNumber := rand.Intn(100) + 1
+
+	
+	if isEven(randomNumber) {
+		fmt.Printf("%d is even\n", randomNumber)
+	} else {
+		fmt.Printf("%d is odd\n", randomNumber)
+	}
 }
+
+func isEven(num int) bool {
+	return num%2 == 0
+}
+In this program, we use the same fmt and math/rand packages as before, but we generate a random number between 1 and 100 using rand.Intn(100) + 1 instead of rand.Intn(10). We then call a custom function isEven to check if the number is even or odd, and print a message accordingly. This is a different method than the original code, which simply printed the random number.
+
+
+
+
+
+
