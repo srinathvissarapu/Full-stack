19d94a5cbadb1587ab47cf08b6891c9543f79820
srinathvissarapu <119414830+srinathvissarapu@users.noreply.github.com>
2023-03-19 11:29:27 +0530

    Update 16.go

--- 16.go
+++ 16.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 const (
-	// Create a huge number by shifting a 1 bit left 100 places.
-	// In other words, the binary number that is 1 followed by 100 zeroes.
-	Big = 1 << 100
-	// Shift it right again 99 places, so we end up with 1<<1, or 2.
-	Small = Big >> 99
+	// Create a large number by shifting a 1 bit left 50 places.
+	// In other words, the binary number that is 1 followed by 50 zeroes.
+	Large = 1 << 50
+	// Shift it right again 49 places, so we end up with 1<<1, or 2.
+	Small = Large >> 49
 )
 
-func needInt(x int) int { return x*10 + 1 }
+func needInt(x int) int { return x*5 + 2 }
 func needFloat(x float64) float64 {
-	return x * 0.1
+	return x * 0.2
 }
 
 func main() {
 	fmt.Println(needInt(Small))
 	fmt.Println(needFloat(Small))
-	fmt.Println(needFloat(Big))
+	fmt.Println(needFloat(Large))
 }
