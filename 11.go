f3a7537f59a475290ca643f0ce609ed541d307d2
srinathvissarapu <119414830+srinathvissarapu@users.noreply.github.com>
2023-03-19 11:24:24 +0530

    Update 11.go

--- 11.go
+++ 11.go
@@ -6,9 +6,9 @@ import (
 )
 
 var (
-	ToBe   bool       = false
-	MaxInt uint64     = 1<<64 - 1
-	z      complex128 = cmplx.Sqrt(-5 + 12i)
+	ToBe   bool       = true
+	MaxInt uint64     = 42
+	z      complex128 = cmplx.Sqrt(-7 + 24i)
 )
 
 func main() {
