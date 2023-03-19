b12f351c4d8b4e9a2c07979b88b1f7b36c741f3e
srinathvissarapu <119414830+srinathvissarapu@users.noreply.github.com>
2023-03-19 11:23:11 +0530

    Update 10.go

--- 10.go
+++ 10.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var i, j int = 1, 2
-	k := 3
-	c, python, java := true, false, "no!"
+	var i, j int = 10, 20
+	k := 30
+	c, python, java := false, true, "yes!"
 
 	fmt.Println(i, j, k, c, python, java)
 }
